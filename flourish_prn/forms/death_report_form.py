cbb9c8442d877a95b5b082705acf75e5f466bbda
MoffatMore <mofenyimoffat@gmail.com>
2021-11-18 15:06:18 +0200

    fix death report form

--- flourish_prn/forms/death_report_form.py
+++ flourish_prn/forms/death_report_form.py
@@ -1,11 +1,15 @@
 from django import forms
 from edc_form_validators import FormValidatorMixin
-
+from flourish_form_validations.form_validators.form_validator_mixin import (
+    FlourishFormValidatorMixin)
 from ..models import DeathReport
 
 
-class DeathReportForm(FormValidatorMixin, forms.ModelForm):
+class DeathReportForm(FormValidatorMixin, FlourishFormValidatorMixin, forms.ModelForm):
 
+    subject_identifier = forms.CharField(
+        label='Subject Identifier',
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}))
 
     def clean(self):
         self.subject_identifier = self.cleaned_data.get(
--- flourish_prn/models/death_report.py
+++ flourish_prn/models/death_report.py
@@ -1,8 +1,12 @@
+from django.db import models
 from edc_action_item.model_mixins.action_model_mixin import ActionModelMixin
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
+from edc_base.model_validators.date import datetime_not_future
 from edc_base.sites import SiteModelMixin
+from edc_base.utils import get_utcnow
 from edc_identifier.managers import SubjectIdentifierManager
+from edc_protocol.validators import datetime_not_before_study_start
 from edc_search.model_mixins import SearchSlugModelMixin
 
 from ..action_items import DEATH_REPORT_ACTION
@@ -16,6 +20,15 @@ class DeathReport(ActionModelMixin,
     tracking_identifier_prefix = 'MO'
 
     action_name = DEATH_REPORT_ACTION
+
+    report_datetime = models.DateTimeField(
+        verbose_name="Report Date",
+        validators=[
+            datetime_not_before_study_start,
+            datetime_not_future],
+        default=get_utcnow,
+        help_text=('If reporting today, use today\'s date/time, otherwise use'
+                   ' the date/time this information was reported.'))
 
     objects = SubjectIdentifierManager()
 
