6934948bc92cbe424c1b69c66ae0791a76000402
MoffatMore <mofenyimoffat@gmail.com>
2021-12-06 16:31:30 +0200

    fix child death report

--- flourish_prn/forms/child_death_report_form.py
+++ flourish_prn/forms/child_death_report_form.py
@@ -3,6 +3,8 @@ from edc_form_validators import FormValidatorMixin
 from flourish_form_validations.form_validators.form_validator_mixin import (
     FlourishFormValidatorMixin)
 from ..models import ChildDeathReport
+from django.core.exceptions import ValidationError
+
 
 
 class ChildDeathReportForm(FormValidatorMixin, FlourishFormValidatorMixin, forms.ModelForm):
@@ -17,6 +19,22 @@ class ChildDeathReportForm(FormValidatorMixin, FlourishFormValidatorMixin, forms
         self.validate_against_consent_datetime(
             self.cleaned_data.get('report_datetime'))
         super().clean()
+
+    def validate_against_consent(self):
+        """Returns an instance of the current maternal consent version form or
+        raises an exception if not found."""
+        subject_identifier = self.subject_identifier[:-3]
+
+        try:
+            consent = self.caregiver_consent_cls.objects.get(
+                subject_identifier=subject_identifier,
+                version='1')
+        except self.caregiver_consent_cls.DoesNotExist:
+                raise ValidationError(
+                    'Please complete Caregiver Consent form '
+                    f'before proceeding.')
+        else:
+            return consent
 
     class Meta:
         model = ChildDeathReport
