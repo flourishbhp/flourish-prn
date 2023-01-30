8a75d139b84da69430ec4d84cabab67f1e6e8c79
Moses Chawawa <moseschawawa@gmail.com>
2023-01-30 15:49:49 +0200

    children dob

--- flourish_prn/admin/exportaction_mixin.py
+++ flourish_prn/admin/exportaction_mixin.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 import xlwt
-
+from django.apps import apps as django_apps
 
 class ExportActionMixin:
 
@@ -27,16 +27,27 @@ class ExportActionMixin:
         field_names = queryset[0].__dict__
         field_names = [a for a in field_names.keys()]
         field_names.remove('_state')
+        field_names.append('dob')
+
 
         for col_num in range(len(field_names)):
             ws.write(row_num, col_num, field_names[col_num], font_style)
 
         for obj in queryset:
             obj_data = obj.__dict__
+            
+            dob = self.dob_obj(obj_data['subject_identifier'])
+            
+            if dob:
+                obj_data['dob'] = self.dob_obj(obj_data['subject_identifier']).strftime('%Y/%m/%d')
+            else:
+                obj_data['dob'] = 'N/A'
+            
             data = [obj_data[field] for field in field_names]
 
             row_num += 1
             for col_num in range(len(data)):
+                
                 if isinstance(data[col_num], uuid.UUID):
                     ws.write(row_num, col_num, str(data[col_num]))
                 elif isinstance(data[col_num], datetime.datetime):
@@ -51,6 +62,20 @@ class ExportActionMixin:
         'Export selected %(verbose_name_plural)s')
 
     actions = [export_as_csv]
+    
+    def dob_obj(self, subject_identifier: str):
+        consent_cls = django_apps.get_model('flourish_caregiver.subjectconsent')
+        child_consent_cls = django_apps.get_model('flourish_caregiver.caregiverchildconsent')
+         
+        try:
+            consent = consent_cls.objects.filter(
+                subject_identifier=subject_identifier).latest('consent_datetime')
+        except consent_cls.DoesNotExist:
+            consent = child_consent_cls.objects.filter(
+                subject_identifier=subject_identifier).latest('consent_datetime')
+            return consent.child_dob
+        else:
+            return consent.dob
 
     def get_export_filename(self):
         date_str = datetime.datetime.now().strftime('%Y-%m-%d')
