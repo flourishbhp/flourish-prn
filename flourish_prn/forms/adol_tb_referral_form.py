159af096600ee2bbb879306e1d26ad6ec7652e29
ame diphoko <diphokoame@gmail.com>
2023-03-28 13:39:04 +0200

    fix: remove consent validation for adol referral

--- flourish_prn/forms/adol_tb_referral_form.py
+++ flourish_prn/forms/adol_tb_referral_form.py
@@ -18,8 +18,6 @@ class TbReferralAdolForm(FormValidatorMixin, FlourishFormValidatorMixin, forms.M
     def clean(self):
         self.subject_identifier = self.cleaned_data.get('subject_identifier')
 
-        self.validate_against_consent_datetime(
-            self.cleaned_data.get('report_datetime'))
         super().clean()
 
     class Meta:
