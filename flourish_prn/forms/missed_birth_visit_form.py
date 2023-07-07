96ea17c8c24d88f5189a3ce9b29dd1b419cc9d23
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-07-07 14:28:43 +0200

    Refactoring MissedBirthVisit

--- flourish_prn/forms/missed_birth_visit_form.py
+++ flourish_prn/forms/missed_birth_visit_form.py
@@ -6,38 +6,35 @@ from flourish_form_validations.form_validators import FormValidatorMixin as Flou
 from django.apps import apps as django_apps
 
 class MissedBirthVisitForm(FlourishFormValidatorMixin,FormValidatorMixin,forms.ModelForm):
-
+    
     form_validator_cls = MissedBirthVisitFormValidator
+    antenatal_enrol_model= 'flourish_caregiver.antenatalenrollment'
 
     subject_identifier = forms.CharField(
         label='Subject Identifier',
         widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    
+    @property
+    def ultrasound_model_cls(self):
+        return django_apps.get_model(self.antenatal_enrol_model)
+
 
     def __init__(self, *args, **kwargs):
-        super(MissedBirthVisitForm, self).__init__(*args, **kwargs)
 
-        subject_identifier = self.initial.get('subject_identifier', None)
-
-        if not subject_identifier:
-            return
+        super().__init__(*args, **kwargs)
     
-        ultrasound_cls = django_apps.get_model('flourish_caregiver.ultrasound')
+        child_subject_identifier = self.initial.get('subject_identifier', None)
+        maternal_subject_identifier = None
 
-        ultrasound_obj = ultrasound_cls.objects.filter(subject_identifier = subject_identifier)
+        if child_subject_identifier:
+            maternal_subject_identifier = child_subject_identifier[:-3]
 
-        if ultrasound_obj:
+        antenatal_enrol_obj =self.ultrasound_model_cls.objects.filter(subject_identifier = maternal_subject_identifier).first()
+        if antenatal_enrol_obj:
+            if antenatal_enrol_obj.real_time_ga:
+                self.initial['gestational_age'] = antenatal_enrol_obj.real_time_ga
 
-            self.initial['gestation_age'] = ultrasound_obj.ga_by_ultrasound_wks
-
-    def clean(self):
-        self.subject_identifier = self.cleaned_data.get(
-            'subject_identifier')
-        self.validate_against_consent_datetime(
-            self.cleaned_data.get('report_datetime'))
-        super().clean()
-
-
+   
     class Meta:
         model = MissedBirthVisit
         fields = '__all__'
-
