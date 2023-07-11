7d3123f1f51a93a94a6a7c48b125a786dbbbb448
nmunatsibw <mcturnergarai@gmail.com>
2023-07-11 08:07:39 +0200

    :ambulance: fix(Visit Schedule): Fix tb adol off study

--- flourish_prn/models/signals.py
+++ flourish_prn/models/signals.py
@@ -3,7 +3,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from edc_visit_schedule import site_visit_schedules
 
-from flourish_prn.models import TbReferalAdol, ChildOffStudy
+from flourish_prn.models.adol_tb_referral import TbReferalAdol
+from flourish_prn.models.child_off_study import ChildOffStudy
+from flourish_prn.models.tb_adol_off_study import TBAdolOffStudy
 
 
 @receiver(post_save, weak=False, sender=TbReferalAdol,
@@ -43,3 +45,22 @@ def child_offstudy_on_post_save(sender, instance, raw, created, **kwargs):
 
         if fu_notes.exists() and not fu_schedule.exists():
             fu_notes.delete()
+
+
+@receiver(post_save, weak=False, sender=TBAdolOffStudy,
+          dispatch_uid='tb_adol_offstudy_post_save')
+def tb_adol_offstudy_post_save(sender, instance, raw, created, **kwargs):
+    if not raw:
+        tb_schedules = {
+            'tb_adol_schedule': 'flourish_child.onschedulechildtbadolschedule',
+            'tb_adol_followup_schedule': 'flourish_child.onscheduletbadolfollowupschedule'
+        }
+        for schedule_name, onschedule_model in tb_schedules.items():
+            _, schedule = site_visit_schedules.get_by_onschedule_model_schedule_name(
+                onschedule_model=onschedule_model,
+                name=schedule_name)
+            if schedule.is_onschedule(subject_identifier=instance.subject_identifier,
+                                      report_datetime=instance.report_datetime):
+                schedule.take_off_schedule(
+                    subject_identifier=instance.subject_identifier,
+                    schedule_name=schedule_name)
