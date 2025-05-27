270daa58946f05292c24466b4dad306e093eb904
Kebadiretse Neya <motlhabi462@gmail.com>
2025-05-27 10:28:26 +0200

    Refactor the tb_adol validator to accomodate all its children

--- flourish_prn/forms/tb_adol_off_study_form.py
+++ flourish_prn/forms/tb_adol_off_study_form.py
@@ -1,9 +1,12 @@
 from django import forms
+from flourish_prn.form_validations import OffstudyFormValidator
 from flourish_prn.form_validations.tb_adol_offstudy_form_validator import TBAdolOffstudyValidator
 from flourish_prn.models import TBAdolOffStudy
 from edc_form_validators import FormValidatorMixin
 
 class TBAdolOffStudyForm(FormValidatorMixin, forms.ModelForm):
+    OffstudyFormValidator.visit_model = 'flourish_child.childvisit'
+
     form_validator_cls = TBAdolOffstudyValidator
 
     subject_identifier = forms.CharField(
